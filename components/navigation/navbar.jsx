32a42e6271f3492e2b55cd600d9b0d9360a7f04f
Kamil Wróbel <kamilwrobel@MacBook-Air.local>
2024-05-20 22:43:49 +0200

    linkedin bugfix

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <div className={styles.center}>
-                <a href="https://www.linkedin.com">
+                <a href="https://www.linkedin.com/in/kamil-wróbel-28932923a/">
                     <img src="/LinkedIn_logo.svg" alt="LinkedIn Logo" className={styles.linkedin} />
                 </a>
             </div>
