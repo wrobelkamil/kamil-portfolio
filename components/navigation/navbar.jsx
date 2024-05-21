ae60a25f499575b3ed3553b453d77bdea151f9b6
Your Name <you@example.com>
2024-05-21 14:51:55 +0200

    better navbar and bg in herosection

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/Navbar.module.css';
 
 const Navbar = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleHover = () => {
+        setIsHovered(!isHovered);
+    }
+
     return (
-        <nav className={styles.navbar}>
+        <nav className={isHovered ? `${styles.navbar} ${styles.hovered}` : styles.navbar} onMouseEnter={handleHover} onMouseLeave={handleHover}>
             <div className={styles.center}>
                 <a href="https://www.linkedin.com/in/kamil-wróbel-28932923a/">
                     <img src="/LinkedIn_logo.svg" alt="LinkedIn Logo" className={styles.linkedin} />
